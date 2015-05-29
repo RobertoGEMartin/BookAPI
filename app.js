a829f7547418097c2b6fa6b0ca0260ba1b1e349d
@rgem52 <rgomez@hi-iberia.es>
2015-05-29 10:10:27 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Rober on 28/05/15.
+ */
+var express = require('express');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+var bookRouter = express.Router();
+bookRouter.route('/Books')
+    .get(function(req,res){
+        var responseJSON = {hello: "This is my api"};
+        res.json(responseJSON);
+    });
+
+app.use('/api',bookRouter);
+
+app.get('/',function(req,res){
+    res.send('Welcome Bob to my RESTful API¡¡');
+
+});
+
+app.listen(port,function(){
+    console.log('Gulp is running my app on PORT: ' + port);
+});
