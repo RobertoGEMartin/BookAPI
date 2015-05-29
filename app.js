b9ce3bb66dacd2b53ef636a7b3049a4beed25918
@rgem52 <rgomez@hi-iberia.es>
2015-05-29 13:55:16 +0200

    Implement HTTP Post Verb

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
  * Created by Rober on 28/05/15.
  */
 var express = require('express'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
 
 var dbConnection = mongoose.connection;
 var db = mongoose.connect('mongodb://localhost/bookAPI');
@@ -12,41 +13,21 @@ dbConnection.once('open', function (callback) {
     console.log('connected to : mongodb://localhost/bookAPI');
 });
 
-var Book = require('./models/bookModel')
-insertData();
+var Book = require('./models/bookModel');
+//insertData();
+
 var app = express();
+
 var port = process.env.PORT || 3000;
 
-var bookRouter = express.Router();
-bookRouter.route('/Books')
-    .get(function(req,res){
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
-        var query = {};
-
-        if(req.query.genre) {
-            query.genre = req.query.genre;
-        }
-        Book.find(query, function(err,books){
-            if(err)
-                res.status(500).send(err);
-            else
-                res.json(books);
-        });
-    });
-
-bookRouter.route('/Books/:bookId')
-    .get(function(req,res){
+bookRouter = require('./Routes/bookRoutes')(Book);
 
 
-        Book.findById(req.params.bookId, function(err,book){
-            if(err)
-                res.status(500).send(err);
-            else
-                res.json(book);
-        });
-    });
-
-app.use('/api',bookRouter);
+app.use('/api/books',bookRouter);
+//app.use('/api/author',authorRouter);
 
 app.get('/',function(req,res){
     res.send('Welcome Bob to my RESTful API¡¡');
